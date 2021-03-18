089d1013c1d7f4120ac452ca05c7a615bee18892
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-03-18 11:52:20 -0500

    included additional express app handling

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // HTML Routes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
